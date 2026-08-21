5906be914c60c24469d0ace2ab8f1d6fa125de5c
Abel Alemayehu <148069928+Zeordi@users.noreply.github.com>
2026-08-21 23:21:55 +0300

    Update index.ts

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -1,1 +1,8 @@
-
+export * from "./user";
+export * from "./teacher";
+export * from "./vault";
+export * from "./badge";
+export * from "./job";
+export * from "./contract";
+export * from "./attendance";
+export * from "./chat";
