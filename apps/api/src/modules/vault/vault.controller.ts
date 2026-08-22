b97ea12204aa5ae01dc588e0a4b231d83dd64564
Abel Alemayehu <148069928+Zeordi@users.noreply.github.com>
2026-08-22 16:50:55 +0300

    Update vault.controller.ts

--- apps/api/src/modules/vault/vault.controller.ts
+++ apps/api/src/modules/vault/vault.controller.ts
@@ -1,1 +1,20 @@
+import { Controller, Post, Get, UseGuards } from '@nestjs/common';
+import { VaultService } from './vault.service';
 
+@Controller('vault')
+export class VaultController {
+  constructor(private readonly vaultService: VaultService) {}
+
+  // Only Super Admins / Verification Officers can access
+  @Post('upload')
+  async uploadDocument() {
+    // TODO: Accept encrypted upload of Fayda ID, Degree, Liveness selfie
+    return { message: 'Document uploaded to encrypted vault' };
+  }
+
+  @Get(':id')
+  async getDocument() {
+    // TODO: Decrypt and return only to authorized admins
+    return { message: 'Secure document access' };
+  }
+}
