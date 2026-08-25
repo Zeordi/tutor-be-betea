90b00fb4b191945118f40544529ac67ac6061190
Abel Alemayehu <148069928+Zeordi@users.noreply.github.com>
2026-08-25 23:17:54 +0300

    Update vault.module.ts

--- apps/api/src/modules/vault/vault.module.ts
+++ apps/api/src/modules/vault/vault.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VaultController } from './vault.controller';
 import { VaultService } from './vault.service';
+import { AuditModule } from '../audit/audit.module';
 
 @Module({
+  imports: [AuditModule],
   controllers: [VaultController],
   providers: [VaultService],
   exports: [VaultService],
